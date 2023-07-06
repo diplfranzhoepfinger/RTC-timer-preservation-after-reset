fcba1f7fa6aa4968b97bffa69a8e60f65c17aa67
Franz Höpfinger <mail@franzhoepfinger.de>
2023-07-06 13:00:42 +0200

    make a Test

--- project/main/main.c
+++ project/main/main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <time.h>
+#include "esp_log.h"
+
+
+
+static const char * const TAG = "example";
 
 void app_main(void)
 {
+
+
+	time_t now;
+	char strftime_buf[64];
+	struct tm timeinfo;
+
+	time(&now);
+	// Set timezone to China Standard Time
+	setenv("TZ", "CST-8", 1);
+	tzset();
+
+	localtime_r(&now, &timeinfo);
+	strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+	ESP_LOGI(TAG, "The current date/time in Shanghai is: %s", strftime_buf);
+
+
+
+
+    for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
 
 }
